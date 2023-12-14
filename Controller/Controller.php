231dcc06a10228ef9ee364799112ae177a243391
tholibfimbay <127409399+tholibfimbay@users.noreply.github.com>
2023-12-14 13:09:31 +0700

    Add files via upload

---
+++ Controller/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controller;
+class Controller
+{
+    // VARIABLE ATRIBUT
+     var $controllerName; var $controllerMethod;
+    
+     // METHOD UNTUK MENGAMBIL SEMUA ATRIBUT
+    public function getControllerAttribute()
+    {
+        return [
+            "ControllerName" => $this -> controllerName,
+            "Method"=> $this ->controllerMethod 
+        ];
+    }
+}
